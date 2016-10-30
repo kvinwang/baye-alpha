650dca3c2ccc96360b11ae7b52e38feba12bc487
loong <wy721@qq.com>
2016-10-30 19:12:58 +0800

    刷新js缓存

---
+++ updatejsversion.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+__author__ = 'loong'
+"""
+import glob
+import re
+import sys
+
+jsname = sys.argv[1]
+ver = sys.argv[2]
+
+regstr = '({})([0-9]?)'.format(re.escape(jsname + '?ver='))
+
+print(regstr)
+regex = re.compile(regstr)
+
+for filename in glob.glob('./*.html'):
+    content = open(filename).read()
+    open(filename, 'w').write(re.sub(regex, r'\g<1>' + ver, content))
